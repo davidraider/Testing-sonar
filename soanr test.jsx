946881933972bc035aa61cd43ef7748b1e32b7ac
davidraider <73720997+davidraider@users.noreply.github.com>
2024-07-17 16:09:25 +0530

    changed

--- soanr test.jsx
+++ soanr test.jsx
@@ -153,6 +153,7 @@
 
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
+//added new lines
 
 if (typeof JSON !== "object") {
     JSON = {};
